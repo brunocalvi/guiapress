3d4b29295fc9121ec57fd8eb9bf7957469053691
brunocalvi <calvireis@gmail.com>
2023-09-26 22:07:04 -0300

    Iniciando projeto do Blog em node

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database.js");
+const categoriesController = require("./categories/CategoriesController.js");
+const articlesController = require("./articles/ArticlesController.js");
+
+//VIEW ENGINE
+app.set('view engine', 'ejs');
+
+// STATIC
+app.use(express.static('public'));
+
+// BODY PARSER
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// DATABASE
+connection.authenticate()
+  .then(()=>{
+    console.log("Conexão realizada com sucesso!");
+  }).catch((error)=>{
+    console.log(error);
+});
+
+// IMPORTANDO ROTAS
+app.use("/", categoriesController);
+app.use("/", articlesController);
+
+app.get("/", (req, res)=>{
+  res.render("index.ejs");
+});
+
+app.listen(4000, ()=>{
+  console.log("O servidor está rodando na porta 4000!");
+});
